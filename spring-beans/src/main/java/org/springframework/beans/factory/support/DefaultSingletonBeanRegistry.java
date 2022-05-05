834ce2a72f04d5b14dadfe4e49aaef9821fad652
caiqiujun <childrengreens@aliyun.com>
2022-05-05 16:39:57 +0800

    feat: 增加注释

--- spring-beans/src/main/java/org/springframework/beans/factory/support/DefaultSingletonBeanRegistry.java
+++ spring-beans/src/main/java/org/springframework/beans/factory/support/DefaultSingletonBeanRegistry.java
@@ -179,7 +179,9 @@ public class DefaultSingletonBeanRegistry extends SimpleAliasRegistry implements
 	 */
 	protected void addSingletonFactory(String beanName, ObjectFactory<?> singletonFactory) {
 		Assert.notNull(singletonFactory, "Singleton factory must not be null");
+		// 设置三级缓存
 		this.singletonFactories.put(beanName, singletonFactory);
+		// 删除二级缓存
 		this.earlySingletonObjects.remove(beanName);
 		this.registeredSingletons.add(beanName);
 	}
@@ -206,7 +208,10 @@ public class DefaultSingletonBeanRegistry extends SimpleAliasRegistry implements
 	@Nullable
 	protected Object getSingleton(String beanName, boolean allowEarlyReference) {
 		// Quick check for existing instance without full singleton lock.
+		// 根据beanName从缓存中拿实例
+		// 先从一级缓存拿
 		Object singletonObject = this.singletonObjects.get(beanName);
+		// 如果bean还正在创建，还没创建完成，其实就是推内存有厂，属性还没有DI依赖注入
 		if (singletonObject == null && isSingletonCurrentlyInCreation(beanName)) {
 			singletonObject = this.earlySingletonObjects.get(beanName);
 			if (singletonObject == null && allowEarlyReference) {
@@ -216,15 +221,21 @@ public class DefaultSingletonBeanRegistry extends SimpleAliasRegistry implements
 				}
 				try {
 					// Consistent creation of early reference within full singleton lock.
+					// 从二级缓存中拿
 					singletonObject = this.singletonObjects.get(beanName);
+					// 如果还拿不到，并允许bean提前暴露
 					if (singletonObject == null) {
 						singletonObject = this.earlySingletonObjects.get(beanName);
 						if (singletonObject == null) {
+							// 从三级缓存中拿到对象工厂
 							ObjectFactory<?> singletonFactory = this.singletonFactories.get(beanName);
 							if (singletonFactory != null) {
+								// 从工厂中拿到对象
 								singletonObject = singletonFactory.getObject();
 								// Singleton could have been added or removed in the meantime.
+								// 删除三级缓存
 								if (this.singletonFactories.remove(beanName) != null) {
+									// 升级到二级缓存
 									this.earlySingletonObjects.put(beanName, singletonObject);
 								}
 								else {
