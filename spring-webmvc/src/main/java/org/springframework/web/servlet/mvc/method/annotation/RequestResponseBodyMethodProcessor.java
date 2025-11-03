a696df0ef43359ec25c70fc1517b27248cdbf1ba
qiujun_cai <qiujun_cai@meta48.com>
2025-11-03 10:58:48 +0800

    Add comment to highlight core logic in RequestResponseBodyMethodProcessor
    (cherry picked from commit b4f7c765fa167ce590cfa79835b35ba3b1ba91dc)

--- spring-webmvc/src/main/java/org/springframework/web/servlet/mvc/method/annotation/RequestResponseBodyMethodProcessor.java
+++ spring-webmvc/src/main/java/org/springframework/web/servlet/mvc/method/annotation/RequestResponseBodyMethodProcessor.java
@@ -205,6 +205,7 @@ public class RequestResponseBodyMethodProcessor extends AbstractMessageConverter
 		}
 
 		// Try even with null return value. ResponseBodyAdvice could get involved.
+		// 核心逻辑，重点看
 		writeWithMessageConverters(returnValue, returnType, inputMessage, outputMessage);
 	}
 
